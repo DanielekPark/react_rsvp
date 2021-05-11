f3394326805988343d36d6dd2efd69c4fe3b3c18
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2021-05-11 14:01:38 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = [
+  {
+   guestName: 'Guest 1',
+   id: 'helloWord',
+   isConfirmed: true, 
+   isEditing: false
+  },
+  {
+   guestName: 'Guest 2',
+   id: 'lorem',
+   isConfirmed: true,
+   isEditing: true
+  },
+  {
+   guestName: 'Guest 3',
+   id: 'qwerty',
+   isConfirmed: false,
+   isEditing: false
+  }  
+];
+
+export default data
